0b71102786ffd2509cab9b25f0eb3a5bd3e2afb0
馮小高 <jeff79213@gmail.com>
2026-05-22 21:27:01 +0800

    Initial commit: 語音血壓紀錄器 PWA

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = 'bp-recorder-v1';
+const URLS = ['index.html', 'manifest.json', 'icon.svg'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((c) => c.addAll(URLS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((ks) => Promise.all(ks.map((k) => { if (k !== CACHE) return caches.delete(k); }))).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then((cached) => cached || fetch(e.request).then((res) => {
+      if (res && res.ok && res.type === 'basic') {
+        const clone = res.clone();
+        caches.open(CACHE).then((c) => c.put(e.request, clone));
+      }
+      return res;
+    }).catch(() => caches.match('index.html'))
+  ));
+});
